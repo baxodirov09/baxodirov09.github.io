230186c9432e3614183f9251e3de4adf137907dd
Akmal Baxodirov <bahodirov0609@gmail.com>
2020-05-26 18:27:12 +0500

    Updates

---
+++ precache-manifest.39ae33a28a941094d8223d345d94dc4f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc3abcd7df66a197128fe463bd3a1b4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "09557756ced92a7fc381",
+    "url": "/portfolio/static/css/main.bdb80906.chunk.css"
+  },
+  {
+    "revision": "517c062aa8dc69cae866",
+    "url": "/portfolio/static/js/2.69d9d68d.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/portfolio/static/js/2.69d9d68d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09557756ced92a7fc381",
+    "url": "/portfolio/static/js/main.1e061a25.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "a37a440a03d0b2b16bec32fba13646ef",
+    "url": "/portfolio/static/media/IMG_9642.a37a440a.JPG"
+  },
+  {
+    "revision": "9d1de80118399c2e5c9df1afa6317183",
+    "url": "/portfolio/static/media/cfsamuraibobregular-rmg8-webfont.9d1de801.woff2"
+  },
+  {
+    "revision": "a4b8155cc3228a6d41ead3b637a98fbf",
+    "url": "/portfolio/static/media/cfsamuraibobregular-rmg8-webfont.a4b8155c.woff"
+  },
+  {
+    "revision": "100edb035e8b43ccb7dc0e7cf3ba57b6",
+    "url": "/portfolio/static/media/samurai-head-illustration_228886-49.100edb03.jpg"
+  },
+  {
+    "revision": "f79be0f230a9a66a83cba32761fefed5",
+    "url": "/portfolio/static/media/samurai_PNG26.f79be0f2.png"
+  },
+  {
+    "revision": "57e1dbe643bd84f45e4599ffb82af3a7",
+    "url": "/portfolio/static/media/—Pngtree—samurai japanese knife illustration_4236048.57e1dbe6.png"
+  }
+]);
